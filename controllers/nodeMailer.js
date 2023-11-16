4ae5ec418b5e9f7a3d8eaea3470c97ab00c44efa
Nasiruddin Abubakar <111733598+nasiruddinabubakar@users.noreply.github.com>
2023-11-17 00:48:18 +0430

    node mailer with template working

--- controllers/nodeMailer.js
+++ controllers/nodeMailer.js
@@ -1,24 +1,21 @@
 const nodemailer = require("nodemailer");
+const html = require('./template');
 
-const html = `
-  <h1>Hello world</h1>
-  <p1>Isn't NodeMailer awesome?</p>
-`;
 
 async function main() {
   // Create a nodemailer transporter for Gmail
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "nasiruddinabubakar47@gmail.com", // Your Gmail address
-      pass: "kingkhan456", // Your Gmail password or an App Password if you have 2-Step Verification enabled
+      user: "nasiruddinabubakar@gmail.com", // Your Gmail address
+      pass: "powukutzvgkskldn", // Your Gmail password or an App Password if you have 2-Step Verification enabled
     },
   });
 
   // Define the email options
   const mailOptions = {
-    from: "nasiruddinabubakar47@gmail.com", // Sender's email address
-    to: "nasiruddinabubakar@gmail.com", // Recipient's email address
+    from: "nasiruddinabubakar@gmail.com", // Sender's email address
+    to: "k214898@nu.edu.pk", // Recipient's email address
     subject: "Test Email", // Email subject
     html: html, // HTML content of the email
   };
