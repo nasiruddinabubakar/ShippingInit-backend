12423f45a3b1c092e484b30132d32108ab1da50b
Nasiruddin Abubakar <111733598+nasiruddinabubakar@users.noreply.github.com>
2023-12-16 17:44:42 +0430

    .env config

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "../.env" });
 const con = mysql.createConnection({
-  host: process.env.DATABASE || "localhost",
+  host: process.env.DATABASE_HOST || "localhost",
   user: process.env.DATABASE_USER || "root",
   password: process.env.DATABASE_PASSWORD || "",
   database: process.env.DATABASE_NAME || "myDB",
